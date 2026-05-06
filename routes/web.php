f5e9fb51c58e8678d71c96d71897275b0c9f67e9
unknown <yoyomalayalammedia@gmail.com>
2026-05-06 16:11:11 +0530

    route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SystemController;
 
-Route::get('/system/optimize', [SystemController::class, 'optimize']);
+// Route::get('/system/optimize', [SystemController::class, 'optimize']);
 
 Route::get('/{any}', function () {
     return file_get_contents(public_path('index.html'));
